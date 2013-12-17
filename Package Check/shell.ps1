be4bda838be726caab253a3eedd38ae3e63568f9
Vladislav <vlad@holey.org>
2013-12-17 18:11:19 +0300

    Updating search script

--- Package Check/shell.ps1
+++ Package Check/shell.ps1
@@ -1,12 +1,27 @@
-$Search_Patterns = 'regex to search'
-$Search_Patterns = $Search_Patterns.Split(",")
-$Search_Files = 'target'
+$Search_Folder = 'target'
+
+$Search_Patterns = 'belarus'
+$Search_DeniedFileExt = ('.pdb', '.swf')
+$Search_ExcludeFiles = ("*.png", "*.gif", "*.dll")
+
 $Result = @{}
 
-Get-ChildItem $Search_Files -force -recurse -file  | ForEach-Object {
-	$FullName = $_.FullName;
-	$Content = Get-Content -Path $FullName;
+if ($Search_Patterns) { $Search_Patterns = $Search_Patterns.Split(",") }
+if ($Search_DeniedFilePattrns) { $Search_DeniedFilePattrns = $Search_DeniedFilePattrns.Split(",") }
 
+Get-ChildItem $Search_Folder -Force -Recurse -File -Exclude $Search_ExcludeFiles | ForEach-Object {
+	$FullName = $_.FullName;
+	$Ext = [System.IO.Path]::GetExtension($FullName);
+	$Content = Get-Content -Path $FullName;
+	
+	$Search_DeniedFileExt | ForEach {
+		if ($Ext -eq $_)
+		{
+			echo ''
+			echo (' ! ' + $FullName + ' - *' + $_)
+		}
+	}
+	
 	$Search_Patterns | ForEach {
 		$token = "$_".Trim()
 		
