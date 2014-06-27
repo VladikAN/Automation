1e3984a3d9cdc29321dc16dfc31afc4fa4a8fbd5
Vladislav <vlad@holey.org>
2014-06-27 19:00:47 +0300

    Adding database backup code

--- Job-DB-Backup/core.ps1
+++ Job-DB-Backup/core.ps1
@@ -1,9 +1,9 @@
-function Check-Directory
+function Job-DB-Backup
 {
 	[CmdletBinding()]
 	param(
         [Parameter(Mandatory=$true)]
-        [string]$DB_Server = .\SQLEXP,
+        [string]$DB_Server = '.\SQLEXP',
 
         [Parameter(Mandatory=$true)]
         [string]$DB_Name = 'Database_1',
@@ -23,12 +23,18 @@ function Check-Directory
         Add-PSSnapin SqlServerCmdletSnapin100
         Add-PSSnapin SqlServerProviderSnapin100
 
-        $raw_db_name = (@($DB_Backup_Prefix, $DB_Name, $DB_Backup_Suffix) | Where-Object { $_ -ne $null }) -join '_').ToString().ToLower()
-        $raw_db_name_regex = "$($raw_db_name).*?\.bak";
+        $raw_db_name = ((@($DB_Backup_Prefix, $DB_Name, $DB_Backup_Suffix) | Where-Object { $_ -ne $null }) -join '_').ToString().ToLower()
+        $raw_db_date = (Get-Date -format "ddMMyyyy_HHmm")
+
+        $raw_db_name_regex = "$($raw_db_name).*?\.bak"
 	}
 
 	process
 	{
+        $backup_name = "$($raw_db_name)_$($raw_db_date).bak"
+        $backup_query = "BACKUP DATABASE [$($DB_Name)] TO DISK = N'$($DB_Backup_Location)$($backup_name)' WITH NOFORMAT, INIT, NAME = N'$($backup_name)', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+
+        Invoke-Sqlcmd -ServerInstance $DB_Server -Query $backup_query
 	}
 
 	end
